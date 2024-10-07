f2cf86bb223d7f9b4af1d360fd3b73ea727f1117
Adam Passachin <adampassachin@Adams-MacBook-Pro.local>
2024-10-07 22:28:04 +0200

    added timezone functionality

--- src/components/GameItem.jsx
+++ src/components/GameItem.jsx
@@ -6,8 +6,8 @@ function GameItem({ game }) {
     // Convert the date to a more readable format
     function convertTime(fullDate) {
         const date = new Date(fullDate);
-        const hours = date.getUTCHours().toString().padStart(2, '0');
-        const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
         return `${hours}:${minutes}`;
     }
 
