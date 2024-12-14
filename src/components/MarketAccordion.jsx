4776a09297e175aca805063f805bba2cd14511a6
Adam Passachin <adampassachin@Adams-MacBook-Pro.local>
2024-12-14 21:50:16 +0100

    render all bookmakers, fixed odds layout and table border

--- src/components/MarketAccordion.jsx
+++ src/components/MarketAccordion.jsx
@@ -1,4 +1,5 @@
 function MarketAccordion({ market, homeTeam, awayTeam }) {
+    console.log(market);
 
     const replaceTeamNames = (value) => {
         if (typeof value === 'string') {
@@ -9,6 +10,25 @@ function MarketAccordion({ market, homeTeam, awayTeam }) {
         return value;
     };
 
+    const bookmakersList = [
+        "NordicBet",
+        "10Bet",
+        "William Hill",
+        "Bet365",
+        "Marathonbet",
+        "Unibet",
+        "Betfair",
+        "Betsson",
+        "Fonbet",
+        "Pinnacle",
+        "SBO",
+        "1xBet",
+        "Betano",
+        "Betway",
+        "Tipico",
+        "Dafabet"
+    ];
+
     return (
         <div className="collapse collapse-arrow bg-white text-black">
             <input type="radio" name="accordion" id="accordion-market" />
@@ -16,15 +36,16 @@ function MarketAccordion({ market, homeTeam, awayTeam }) {
                 {market[0]?.name}
             </div>
             <div className="collapse-content">
-                <table className="table-fixed w-full border-collapse ">
+                <table className="table-fixed w-full border-collapse">
                     <thead>
-                        <tr className="border-b border-gray-200">
-                            <th className="p-1 text-sm w-1/4"></th>
-                            {market.map((bookmaker) => (
-                                <th key={bookmaker.id} className="p-1 text-sm w-1/4">
+                        <tr>
+                            <th className="p-1 text-sm w-1/4 border-b border-gray-200"></th>
+                            <th className="p-1 text-sm w-1/4 border-b border-gray-200"></th> {/* Empty column to offset */}
+                            {bookmakersList.map((bookmaker) => (
+                                <th key={bookmaker} className="p-1 text-sm w-1/4">
                                     <div className="h-28 flex items-center justify-center">
-                                        <span className="block whitespace-nowrap transform  text-center mx-auto px-4">
-                                            {bookmaker.bookmakerName}
+                                        <span className="block whitespace-nowrap transform text-center mx-auto px-4 rotate-90">
+                                            {bookmaker}
                                         </span>
                                     </div>
                                 </th>
@@ -33,17 +54,18 @@ function MarketAccordion({ market, homeTeam, awayTeam }) {
                     </thead>
                     <tbody>
                         {market[0]?.values.map((value) => (
-                            <tr key={value.value} className="border-b border-gray-200">
-                                <td className="p-1 text-sm truncate">
+                            <tr key={value.value}>
+                                <td className="p-1 text-sm border-b border-gray-200">
                                     {replaceTeamNames(value.value)}
                                 </td>
-                                {market.map((bookmaker) => {
-                                    const oddValue = bookmaker.values.find((v) => v.value === value.value);
+                                <td className="p-1 text-sm border-b border-gray-200"></td> {/* Empty cell to offset */}
+                                {bookmakersList.map((bookmaker) => {
+                                    const oddValue = market.find((b) => b.bookmakerName === bookmaker)?.values.find((v) => v.value === value.value);
                                     return (
-                                        <td key={bookmaker.id} className="text-center p-4 text-sm w-1/4">
+                                        <td key={bookmaker} className="text-center text-sm w-1/4">
                                             {oddValue ? (
                                                 <div className="flex justify-center items-center w-full h-full">
-                                                    <div className="bg-[#DFFAFF] border rounded w-full h-full shadow-md p-2 transition-transform transform hover:scale-104 hover:shadow-lg">
+                                                    <div className="bg-[#DFFAFF] border rounded w-full h-full flex items-center justify-center shadow-md p-2 transition-transform transform hover:scale-110 hover:shadow-lg">
                                                         {oddValue.odd}
                                                     </div>
                                                 </div>
@@ -64,4 +86,3 @@ function MarketAccordion({ market, homeTeam, awayTeam }) {
 }
 
 export default MarketAccordion;
-
